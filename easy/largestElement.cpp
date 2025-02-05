0fc366e82e6864bd0ddb3ffc38022684559a7e3a
Manu04Tiwari <marttiwari8219@gmail.com>
2025-02-05 11:27:21 +0530

    second commit

--- easy/largestElement.cpp
+++ easy/largestElement.cpp
@@ -1,1 +1,19 @@
 #include <bits/stdc++.h>
+using namespace std;
+
+int largestElement(vector<int>& arr, int n) {
+    int largest = arr[0];
+    for (int i = 0; i < n; i++) {
+        if (arr[i] > largest) {
+            largest = arr[i];
+        }
+    }
+    return largest;
+}
+
+int main() {
+    vector<int> arr = {1, 2, 3, 6, 5, 8};
+    int largest = largestElement(arr, arr.size());
+    cout << "The largest element is: " << largest << endl;
+    return 0;
+}
